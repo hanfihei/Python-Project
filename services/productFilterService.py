daf2a13ffcfcc2f0047634fd3ad72a1a7803c4a8
jihei <han01031@naver.com>
2026-05-04 19:35:34 +0900

    Refactor: 불필요한 코드 제거

--- services/productFilterService.py
+++ services/productFilterService.py
@@ -1,15 +1,4 @@
 from data.products import products
-# 상품리스트를 보면서 조건에 안 맞는건 탈락시키고 끝까지 살아남는 상품만 모으기
-# ex) "무게가 1키로가 안 넘고 가격이 50만원이 넘지않는 휴대성 좋은 노트북 추천해줘"
-
-#지피티가 준 json 예시
-# 필터 조건이 없다면 기본 값 반환함 -> int = 0, float = 0.0, str = ""
-# {
-#   "category": "노트북",
-#   "max_price": 500000,
-#   "max_weight": 1.0,
-#   "tags": ["휴대성"] //이거 뺌
-# } 우선 가격이나 무게가 ~이상 일때는 제외함
 
 # products는 더미데이터, filter는 ai가 넘겨준 json자료
 def filter_products(products, filters): 
@@ -17,9 +6,6 @@ def filter_products(products, filters):
 
     for product in products:
         # 딕셔너리는 .으로 접근 불가능
-        # if(product["category"] == filters["category"]
-        #     and product["weight"] <= filters["weight"]
-        #     and product["price"] <= filters["price"]):
         if(filters["category"] != ""):
             if (product["category"] != filters["category"]):
                 continue
